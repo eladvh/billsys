ad61968f852402d333660579fbf651cb242ab05b
Eladvh <eladvh@gmail.com>
2018-12-07 12:00:30 +0200

    finished auth flow

--- services/passport.js
+++ services/passport.js
@@ -3,7 +3,17 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-const user = mongoose.model('users');
+const User = mongoose.model('users');
+
+passport.serializeUser((user,done) =>{
+    done(null,user.id);
+})
+
+passport.deserializeUser((id,done) =>{
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
 
 passport.use(
     new GoogleStrategy(
@@ -13,7 +23,16 @@ passport.use(
             callbackURL: '/auth/google/callback'
         }, 
         (accessToken, refreshToken, profile, done) => {
-            new user({ googleID: profile.id }).save();
+            User.findOne({ googleId: profile.id})
+            .then((existingUser) => {
+                if(existingUser){
+                    console.log(existingUser + "User already exist");
+                    done(null, existingUser);
+                }else{
+                    new User({ googleId: profile.id }).save()
+                    .then(user => done(null, user));
+                }
+            });
        }
     )
 );
